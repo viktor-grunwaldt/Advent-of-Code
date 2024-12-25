b839d953075a5165f5f5c789c36891180865ffd7
Viktor Grunwaldt <v.gruenwaldt@protonmail.com>
2024-12-25 16:40:49 +0100

    use one global flake

--- flake.nix
+++ flake.nix
@@ -14,23 +14,23 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        pdbpp = let 
-          pname = "pdbpp";
-          version = "0.10.3";
-        in pkgs.python312Packages.buildPythonPackage {
-          inherit pname version;
-          src = pkgs.fetchPypi {
-            inherit pname version;
-            sha256 = "sha256-2eQ/T9o4jus2XyiH9Oe2asCdzptiNrdvY2FlMOL2afU=";
-          };
-          doCheck = false;
-          # # specific to buildPythonPackage, see its reference
-          # pyproject = true;
-          # build-system = [
-          #   pkgs.python312Packages.setuptools
-          #   pkgs.python312Packages.wheel
-          # ];
-        };
+        # pdbpp = let 
+        #   pname = "pdbpp";
+        #   version = "0.10.3";
+        # in pkgs.python312Packages.buildPythonPackage {
+        #   inherit pname version;
+        #   src = pkgs.fetchPypi {
+        #     inherit pname version;
+        #     sha256 = "sha256-2eQ/T9o4jus2XyiH9Oe2asCdzptiNrdvY2FlMOL2afU=";
+        #   };
+        #   doCheck = false;
+        #   # # specific to buildPythonPackage, see its reference
+        #   # pyproject = true;
+        #   # build-system = [
+        #   #   pkgs.python312Packages.setuptools
+        #   #   pkgs.python312Packages.wheel
+        #   # ];
+        # };
       in
       {
         devShells.default = with pkgs; mkShell {
