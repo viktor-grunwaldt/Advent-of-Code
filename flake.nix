74b3a57e8cb069972ea870e9680510d2945e741f
Viktor Grunwaldt <v.gruenwaldt@protonmail.com>
2025-12-05 23:41:51 +0100

    update: flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Flake to manage python workspace";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
@@ -14,33 +14,19 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        # pdbpp = let 
-        #   pname = "pdbpp";
-        #   version = "0.10.3";
-        # in pkgs.python312Packages.buildPythonPackage {
-        #   inherit pname version;
-        #   src = pkgs.fetchPypi {
-        #     inherit pname version;
-        #     sha256 = "sha256-2eQ/T9o4jus2XyiH9Oe2asCdzptiNrdvY2FlMOL2afU=";
-        #   };
-        #   doCheck = false;
-        #   # # specific to buildPythonPackage, see its reference
-        #   # pyproject = true;
-        #   # build-system = [
-        #   #   pkgs.python312Packages.setuptools
-        #   #   pkgs.python312Packages.wheel
-        #   # ];
-        # };
       in
       {
         devShells.default = with pkgs; mkShell {
           packages = [
-            (python312.withPackages (ppg: [
+            (python313.withPackages (ppg: [
               ppg.more-itertools
-              ppg.icecream
+              # ppg.icecream
             ]))
             pyright
             ruff
+            ocaml
+            ocamlPackages.ocaml-lsp
+            ocamlPackages.utop
             # too hard
             # pdbpp
           ];
